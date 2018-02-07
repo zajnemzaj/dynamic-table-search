26186a0aabcf2f60de0e15ff82d3e200cf54d6b1
Unknown <dezsoj@gmail.com>
2018-02-07 20:23:54 +0100

    Fixed CSV, reading to array of objects works

--- used-items.php
+++ used-items.php
@@ -15,23 +15,52 @@
     <!-- end of wrapper -->
     <script src="../../js/menu-selector.js"></script>
     <script>
-        function splitText(allText) {
-            // console.log(typeof allText);
-            var lines = allText.split(/\n/);
-            console.log(lines);
-            return 0;
+        function splitTextToLines(allText) {
+            var finalText = "";
+            var start = 0;
+            // swap \n with <br>
+            for (var i = 0; i < allText.length; i++) {
+                if (allText[i] === "\n" && allText[i - 1] != '"') {
+                    finalText += allText.substring(start, i);
+                    finalText += "<br>";
+                    start = i + 1;
+                }
+            }
+            finalText += allText.substring(start, allText.length);
+            var lines = finalText.split(/\n/);
+            return lines;
         }
 
         function readTextFile(file) {
+            var linesArray = [];
             var rawFile = new XMLHttpRequest();
             rawFile.open("GET", file, true);
             rawFile.onreadystatechange = function() {
                 if (rawFile.readyState === 4) {
                     if (rawFile.status === 200 || rawFile.status == 0) {
                         var allText = rawFile.responseText;
-                        console.log(typeof allText);
-                        splitText(allText);
-                        return 0;
+                        var lines = splitTextToLines(allText);
+                        // going through lines
+                        for (var count = 1; count < lines.length; count++) {
+                            var rowObject = {};
+                            // initializing rowContent array to store each column as an array
+                            var rowContent = lines[count].split("|");
+                            // if it's not a empty row
+                            if (rowContent != "") {
+                                // going through columns
+                                for (var i = 0; i < rowContent.length; i++) {
+                                    // getting column names from first line
+                                    var columnName = lines[0].split("|")[i];
+                                    // adding new propery name (by column name) and value (from actual row) to rowObject
+                                    rowObject[columnName] = rowContent[i].replace(/"/g, "");
+                                }
+                                // console.log(rowObject);
+                                // pushing actual rowObject to linesArray
+                                linesArray.push(rowObject);
+                            }
+                        }
+                        console.table(linesArray);
+                        //return linesArray;
                     }
                 }
             }
